dab457f7c06ec1047acee314373273f699ea3e9f
adamtefft <adam_tefft1@baylor.edu>
2023-03-02 20:12:59 -0600

    updated

--- EC-TestScoresGUI.py
+++ EC-TestScoresGUI.py
@@ -8,54 +8,96 @@ class myGUI:
         self.main_window = tkinter.Tk()
         self.main_window.geometry("300x200")
         self.main_window.title("Average Test Scores")
+        self.main_window.config(bg="Green")
 
         self.top_frame = tkinter.Frame(self.main_window)
         self.top_frame.pack()
+        self.top_frame.config(bg="Green")
 
         self.middle_frame = tkinter.Frame(self.main_window)
         self.middle_frame.pack()
+        self.middle_frame.config(bg="Green")
 
         self.bottom_frame = tkinter.Frame(self.main_window)
         self.bottom_frame.pack()
+        self.bottom_frame.config(bg="Green")
 
         self.average_frame = tkinter.Frame(self.main_window)
         self.average_frame.pack()
+        self.average_frame.config(bg="Green")
 
         self.button_frame = tkinter.Frame(self.main_window)
         self.button_frame.pack()
+        self.button_frame.config(bg="Green")
 
-        self.test1_label = tkinter.Label(self.top_frame, text='Enter the score for test 1:')
+        self.test1_label = tkinter.Label(
+            self.top_frame, text='Enter the score for test 1:')
         self.test1_label.pack(side='left')
+        self.test1_label.config(font=("Courier", 10))
+        self.test1_label.config(fg="Yellow")
+        self.test1_label.config(bg="Green")
 
         self.test1_entry = tkinter.Entry(self.top_frame, width=10)
-        self.test1_entry.pack(side = 'right')
+        self.test1_entry.pack(side='right')
+        self.test1_entry.config(fg="Yellow")
+        self.test1_entry.config(bg="Green")
+        self.test1_entry.config(highlightbackground="Yellow")
 
-        self.test2_label = tkinter.Label(self.middle_frame, text='Enter the score for test 2:')
+        self.test2_label = tkinter.Label(
+            self.middle_frame, text='Enter the score for test 2:')
         self.test2_label.pack(side='left')
+        self.test2_label.config(font=("Courier", 10))
+        self.test2_label.config(fg="Yellow")
+        self.test2_label.config(bg="Green")
 
         self.test2_entry = tkinter.Entry(self.middle_frame, width=10)
-        self.test2_entry.pack(side = 'right')    
+        self.test2_entry.pack(side='right')
+        self.test2_entry.config(fg="Yellow")
+        self.test2_entry.config(bg="Green")
+        self.test2_entry.config(highlightbackground="Yellow")
 
-        self.test3_label = tkinter.Label(self.bottom_frame, text='Enter the score for test 3:')
+        self.test3_label = tkinter.Label(
+            self.bottom_frame, text='Enter the score for test 3:')
+        self.test3_label.config(font=("Courier", 10))
+        self.test3_label.config(fg="Yellow")
+        self.test3_label.config(bg="Green")
+
         self.test3_label.pack(side='left')
 
         self.test3_entry = tkinter.Entry(self.bottom_frame, width=10)
-        self.test3_entry.pack(side = 'right')
+        self.test3_entry.pack(side='right')
+        self.test3_entry.config(fg="Yellow")
+        self.test3_entry.config(bg="Green")
+        self.test3_entry.config(highlightbackground="Yellow")
 
-        self.display_average_label = tkinter.Label(self.average_frame, text="Average")
+        self.display_average_label = tkinter.Label(
+            self.average_frame, text="Average")
         self.display_average_label.pack(side='left')
+        self.display_average_label.config(fg="Yellow")
+        self.display_average_label.config(bg="Green")
+        self.display_average_label.config(font=("Courier", 10))
 
         self.display_average = tkinter.StringVar()
 
-        self.average_label = tkinter.Label(self.average_frame, textvariable=self.display_average)
+        self.average_label = tkinter.Label(
+            self.average_frame, textvariable=self.display_average)
         self.average_label.pack(side='left')
+        self.average_label.config(bg="Green")
 
-        self.average_button = tkinter.Button(self.button_frame, text = "Average", command=self.average)
-        self.average_button.pack(side = 'left')
+        self.average_button = tkinter.Button(
+            self.button_frame, text="Average", command=self.average)
+        self.average_button.pack(side='left')
+        self.average_button.config(fg="Green")
+        self.average_button.config(font=("Courier", 10))
+        self.average_button.config(bg="Green")
+        self.average_button.config(highlightbackground="Green")
 
-        self.quit_button = tkinter.Button(self.button_frame, text="Quit", command=self.main_window.destroy)
-        self.quit_button.pack(side = 'right')
-
+        self.quit_button = tkinter.Button(
+            self.button_frame, text="Quit", command=self.main_window.destroy)
+        self.quit_button.pack(side='right')
+        self.quit_button.config(fg="Green")
+        self.quit_button.config(font=("Courier", 10))
+        self.quit_button.config(highlightbackground="Green")
 
         tkinter.mainloop()
 
@@ -66,9 +108,10 @@ class myGUI:
         # These take the entrys from the method above and assigns them to variables in order to calculate the average
 
         test_total = test1 + test2 + test3
-        test_average = round(test_total / 3,2)
+        test_average = round(test_total / 3, 2)
 
         self.display_average.set(test_average)
-        #tkinter.messagebox.showinfo("Title of the messagebox",f"Average: {test_average}")
+        # tkinter.messagebox.showinfo("Title of the messagebox",f"Average: {test_average}")
+
 
 myinstance = myGUI()
--- GUI_Homework.py
+++ GUI_Homework.py
@@ -1,8 +1,9 @@
 import tkinter
 import tkinter.messagebox
 
+
 class myGUI:
-    
+
     def __init__(self):
 
         self.main_window = tkinter.Tk()
@@ -30,75 +31,86 @@ class myGUI:
         self.cb2 = tkinter.Frame(self.main_window)
         self.cb2.pack()
 
-        self.main_text = tkinter.Label(self.email_frame, text='Responsive Registration\nForm\n', font = ("Bold", 22))
-        self.main_text.pack(side = 'top')
+        self.main_text = tkinter.Label(
+            self.email_frame, text='Responsive Registration\nForm\n', font=("Bold", 22))
+        self.main_text.pack(side='top')
 
         self.email_label = tkinter.Label(self.email_frame, text='Email:')
-        self.email_label.pack(side='left')        
+        self.email_label.pack(side='left')
 
         self.email_entry = tkinter.Entry(self.email_frame, width=35)
-        self.email_entry.pack(side = 'right')
+        self.email_entry.pack(side='right')
 
-        self.password_label = tkinter.Label(self.password_frame, text='Password:')
-        self.password_label.pack(side='left')        
+        self.password_label = tkinter.Label(
+            self.password_frame, text='Password:')
+        self.password_label.pack(side='left')
 
-        self.password_entry = tkinter.Entry(self.password_frame, show="*", width=30)
-        self.password_entry.pack(side = 'right')
+        self.password_entry = tkinter.Entry(
+            self.password_frame, show="*", width=30)
+        self.password_entry.pack(side='right')
 
-        self.password2_label = tkinter.Label(self.password2_frame, text='Retype Password:')
-        self.password2_label.pack(side='left')        
+        self.password2_label = tkinter.Label(
+            self.password2_frame, text='Retype Password:')
+        self.password2_label.pack(side='left')
 
-        self.password2_entry = tkinter.Entry(self.password2_frame, show="*", width=25)
-        self.password2_entry.pack(side = 'right')
+        self.password2_entry = tkinter.Entry(
+            self.password2_frame, show="*", width=25)
+        self.password2_entry.pack(side='right')
 
         self.fname_label = tkinter.Label(self.name_frame, text='First Name:')
-        self.fname_label.pack(side='left')        
+        self.fname_label.pack(side='left')
 
-        self.fname_entry = tkinter.Entry(self.name_frame, width=9)
-        self.fname_entry.pack(side = 'left')
+        self.fname_entry = tkinter.Entry(self.name_frame, width=10)
+        self.fname_entry.pack(side='left')
 
-        self.lname_entry = tkinter.Entry(self.name_frame, width=9)
-        self.lname_entry.pack(side = 'right')
+        self.lname_entry = tkinter.Entry(self.name_frame, width=10)
+        self.lname_entry.pack(side='right')
 
         self.lname_label = tkinter.Label(self.name_frame, text='Last Name:')
-        self.lname_label.pack(side='right')        
+        self.lname_label.pack(side='right')
 
         self.radio_var = tkinter.IntVar()
-        self.radiobutton1 = tkinter.Radiobutton(self.rb_frame, variable=self.radio_var, text='Male', value=True)
+        self.radiobutton1 = tkinter.Radiobutton(
+            self.rb_frame, variable=self.radio_var, text='Male', value=True)
         self.radiobutton1.pack(side='left')
 
-        self.radiobutton2 = tkinter.Radiobutton(self.rb_frame, variable=self.radio_var, text='Female', value=True)
+        self.radiobutton2 = tkinter.Radiobutton(
+            self.rb_frame, variable=self.radio_var, text='Female', value=True)
         self.radiobutton2.pack(side='left')
 
         self.check_var1 = tkinter.IntVar()
         self.check_var2 = tkinter.IntVar()
 
-        self.checkbutton1 = tkinter.Checkbutton(self.cb1,text = 'I agree with terms and conditions',variable=self.check_var1)
+        self.checkbutton1 = tkinter.Checkbutton(
+            self.cb1, text='I agree with terms and conditions', variable=self.check_var1)
         self.checkbutton1.pack(side='left')
 
-        self.checkbutton2 = tkinter.Checkbutton(self.cb2,text = 'I want to recieve the newsletter', variable=self.check_var2)
-        self.checkbutton2.pack(side= 'left')
+        self.checkbutton2 = tkinter.Checkbutton(
+            self.cb2, text='I want to recieve the newsletter', variable=self.check_var2)
+        self.checkbutton2.pack(side='left')
 
-        self.confirmationbutton = tkinter.Button(self.main_window, text = "Register", command=self.check_credentials)
+        self.confirmationbutton = tkinter.Button(
+            self.main_window, text="Register", command=self.check_credentials)
         self.confirmationbutton.pack()
 
         tkinter.mainloop()
-
-
 
     def check_credentials(self):
         email = self.email_entry.get()
         password = self.password_entry.get()
         password2 = self.password2_entry.get()
-        #male = self.radiobutton1.get()
-        #female = self.radiobutton2.get()
+        # male = self.radiobutton1.get()
+        # female = self.radiobutton2.get()
 
         if len(email) < 3 or len(email) > 30:
-            tkinter.messagebox.showinfo("Text Box Title","Email must be inbetween 3-14 characters")
+            tkinter.messagebox.showinfo(
+                "Text Box Title", "Email must be inbetween 3-14 characters")
         if len(password) < 8:
-            tkinter.messagebox.showinfo("Text Box Title","Password must be at least 8 characters long")
+            tkinter.messagebox.showinfo(
+                "Text Box Title", "Password must be at least 8 characters long")
         if password != password2:
-            tkinter.messagebox.showinfo("Text Box Title","Passwords do not match")
+            tkinter.messagebox.showinfo(
+                "Text Box Title", "Passwords do not match")
         # I need to check to make sure either the Male or Female RadioButton is checked
         '''
         if male != True:
@@ -108,13 +120,14 @@ class myGUI:
         # I need to figure this out
 
         if self.check_var1.get() != 1:
-            tkinter.messagebox.showinfo("Text Box Title","Both boxes must be checked")
+            tkinter.messagebox.showinfo(
+                "Text Box Title", "Both boxes must be checked")
         if self.check_var2.get() != 1:
-            tkinter.messagebox.showinfo("Text Box Title","Both boxes must be checked")
+            tkinter.messagebox.showinfo(
+                "Text Box Title", "Both boxes must be checked")
 
-        tkinter.messagebox.showinfo("Text Box Title","Success!")
-
-
+        else:
+            tkinter.messagebox.showinfo("Text Box Title", "Success!")
 
 
 myinstance = myGUI()
